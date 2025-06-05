528860ebfc5396dadf6b106bcf92cac43bb8a7e3
Muhammad Qasim <qasimmuhammad278@gmail.com>
2025-06-05 16:53:05 +0500

    OK

--- src/HRM_Home/HRHome.jsx
+++ src/HRM_Home/HRHome.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import Sidebar from './Employees/SideBarEmployees'
+import React from 'react';
+import HRimage from './HR.jpg'; // yahan image ka path sahi rakhna!
+import './Sidebar.css';
 
-function Home() {
+function FinanceHome() {
   return (
-    <div>
-     HR Home
+    <div className='photo'>
+      <img src={HRimage} alt="Finance" />
     </div>
-  )
+  );
 }
 
-export default Home
+export default FinanceHome;
